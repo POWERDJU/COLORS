2017d7f75d1079611a7240eeed84f584642272ee
POWERDJU <powerdju@gmail.com>
2026-02-22 21:16:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'coloring-app-v20260222l';
+const CACHE_VERSION = 'coloring-app-v20260222m';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
